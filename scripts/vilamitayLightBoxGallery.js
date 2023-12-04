d1d66659dbf784a815984a49dcf7c148bbef584b
Vladeta <ivanovicvladeta@gmail.com>
2023-12-04 20:29:26 +0100

    First Commit

---
+++ scripts/vilamitayLightBoxGallery.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+  $(".loft-gallery").magnificPopup({
+    delegate: "a", // child items selector, by clicking on it popup will open
+    type: "image",
+    gallery: { enabled: true },
+    // other options
+  });
+});
+
+$(document).ready(function () {
+  $(".galpao-gallery").magnificPopup({
+    delegate: "a", // child items selector, by clicking on it popup will open
+    type: "image",
+    gallery: { enabled: true },
+    // other options
+  });
+});
+
+$(document).ready(function () {
+  $(".casaantiga-gallery").magnificPopup({
+    delegate: "a", // child items selector, by clicking on it popup will open
+    type: "image",
+    gallery: { enabled: true },
+    // other options
+  });
+});
+
+$(document).ready(function () {
+  $(".commonarea-gallery").magnificPopup({
+    delegate: "a", // child items selector, by clicking on it popup will open
+    type: "image",
+    gallery: { enabled: true },
+    // other options
+  });
+});
---
+++ scripts/vilamitayOwlCarousel.js
@@ -0,0 +1,76 @@
+// below is the carousel for accomodations
+const nextIcon = '<img src="../imgs/icons/gallery-right.svg" alt="direito">';
+const previousIcon = '<img src="../imgs/icons/gallery-left.svg" alt="escerda">';
+
+$(document).ready(function () {
+  $(".owl-accom").owlCarousel({
+    margin: 10,
+    dots: false,
+    navText: [previousIcon, nextIcon],
+    responsive: {
+      // breakpoint from 0 up
+      0: {
+        items: 1,
+      },
+      // breakpoint from 480 up
+      700: {
+        items: 2,
+      },
+      // breakpoint from 768 up
+      1200: {
+        items: 4,
+        nav: true,
+      },
+    },
+  });
+});
+
+// Bellow is carousel for common area
+$(document).ready(function () {
+  $(".owl-commonarea").owlCarousel({
+    // loop: true,
+    margin: 10,
+    dots: false,
+    navText: [previousIcon, nextIcon],
+    responsive: {
+      // breakpoint from 0 up
+      0: {
+        items: 1,
+      },
+      // breakpoint from 480 up
+      700: {
+        items: 2,
+      },
+      // breakpoint from 768 up
+      1200: {
+        items: 4,
+        nav: true,
+      },
+    },
+  });
+});
+
+// below is owl carousel for reviews
+$(document).ready(function () {
+  $(".owl-reviews").owlCarousel({
+    // loop: true,
+    margin: 30,
+    dots: false,
+    navText: [previousIcon, nextIcon],
+    responsive: {
+      // breakpoint from 0 up
+      0: {
+        items: 1,
+      },
+      // breakpoint from 480 up
+      700: {
+        items: 2,
+      },
+      // breakpoint from 768 up
+      1200: {
+        items: 3,
+        nav: true,
+      },
+    },
+  });
+});
